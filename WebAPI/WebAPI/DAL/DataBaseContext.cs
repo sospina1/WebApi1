7dedc72b5b7523e366136969b842f3bb0c7cbc11
SEBASTIAN OSPINA GAVIRIA <sebastianospina305330@correo.itm.edu.co>
2024-06-01 17:31:55 -0500

    Congiguration the BD

---
+++ WebAPI/WebAPI/DAL/DataBaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.DAL.Entities;
+
+namespace WebAPI.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        //Así me conecto a la BD por medio de este constructor
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        //Este método que es propio de EF CORE me sirve para configurar unos índices de cada campo de una tabla en BD
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique(); //Aquí creo un índice del campo Name para la tabla Countries
+        }
+
+        #region DbSets
+
+        public DbSet<Country> Countries { get; set; }
+
+        #endregion
+    }
+}
